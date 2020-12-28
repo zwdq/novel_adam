a372796e7a2d5313f49e8b66a478b407cb7c76e1
zwdq <qchannel@126.com>
2020-12-28 19:59:20 +0800

    123

---
+++ 情感分析.py
@@ -0,0 +1,15 @@
+import jieba.analyse
+
+#path = './novels/天龙八部.txt'
+path = './novels/诡秘之主.txt'
+
+fp = open(path,'r',encoding='utf-8')
+content = fp.read()
+try:
+    #jieba.analyse.set_stop_words('停用词表路径')
+    tags = jieba.analyse.extract_tags(content, topK=100, withWeight=True)
+    for item in tags:
+        print(item[0]+'\t'+str(int(item[1]*1000)))
+
+finally:
+    fp.close()

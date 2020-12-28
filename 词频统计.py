2fcac547e78fcda2c025fc04ca3d128defa8c882
zwdq <qchannel@126.com>
2020-12-28 20:29:18 +0800

    123

--- 词频统计.py
+++ 词频统计.py
@@ -7,7 +7,7 @@ fp = open(path,'r',encoding='utf-8')
 content = fp.read()
 try:
     #jieba.analyse.set_stop_words('停用词表路径')
-    tags = jieba.analyse.extract_tags(content, topK=100, withWeight=True)
+    tags = jieba.analyse.extract_tags(content, topK=200, withWeight=True)
     for item in tags:
         print(item[0]+'\t'+str(int(item[1]*1000)))
 
